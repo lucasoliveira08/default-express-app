42d610e49053c653ddb729925aede2f8d5e7747b
HenriqueRamos13 <henrique.mrcr@gmail.com>
2022-04-27 23:31:39 +0100

    add mongo

---
+++ src/config/db/Mongo.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+class Mongo {
+  constructor() {
+    this.connect();
+  }
+
+  private async connect(): Promise<void> {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log("Mongo connected");
+  }
+}
+
+export default new Mongo();
