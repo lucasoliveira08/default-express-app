14148984e42e2bf4f8c0f575379174fa6453804e
HenriqueRamos13 <henrique.mrcr@gmail.com>
2022-04-27 10:45:19 +0100

    separe auth from session

--- src/controllers/Auth.controller.ts
+++ src/controllers/Auth.controller.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import AuthService from "../services/Auth.service";
-import * as jwt from "jsonwebtoken";
-import "../config/passport";
-import * as passport from "passport";
+import PassportController from "./Passport.controller";
 
 class AuthController {
   private route = Router();
@@ -10,64 +8,19 @@ class AuthController {
   private service = new AuthService();
 
   constructor() {
-    this.get();
-    this.post();
     this.put();
     this.patch();
     this.delete();
-    this.signUp();
-  }
-
-  private get(): void {
-    this.route.get(`${this.defaultURL}`, async (req, res) => {
-      res.json({
-        message: "GET",
-      });
-    });
-  }
-
-  private signUp(): void {
-    this.route.post(
-      `${this.defaultURL}/signup`,
-      passport.authenticate("signup", { session: false }),
-      async (req, res) => {
-        res.json({
-          message: "SIGNUP",
-        });
-      }
-    );
+    this.post();
   }
 
   private post(): void {
     this.route.post(
       `${this.defaultURL}`,
-      async (req, res, next) => {
-        passport.authenticate("sign", async (err, user, info) => {
-          try {
-            if (err || !user) {
-              const error = new Error("An error occurred." + err);
-
-              return next(error);
-            }
-
-            req.login(user, { session: false }, async (error) => {
-              if (error) return next(error);
-
-              const body = { _id: 123, email: user.email };
-              const token = jwt.sign({ user: body }, "TOP_SECRET");
-
-              return res.json({ token });
-            });
-          } catch (error) {
-            return next(error);
-          }
-        })(req, res, next);
-      },
+      PassportController.signup,
       async (req, res) => {
-        this.service.login();
-
         res.json({
-          message: "LOGIN",
+          message: "SIGNUP",
         });
       }
     );
